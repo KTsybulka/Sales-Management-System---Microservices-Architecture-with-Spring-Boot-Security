03c493bdf81c39992274a8191adfd479a9e5443a
KTsybulka <ktsybulka11@gmail.com>
2024-12-05 18:58:06 -0500

    Initialize Customer Role Service microservice

---
+++ customer-role-service/src/main/java/com/example/customer_role_service/CustomerRoleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_role_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRoleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRoleServiceApplication.class, args);
+	}
+
+}
---
+++ customer-role-service/src/test/java/com/example/customer_role_service/CustomerRoleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_role_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRoleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
