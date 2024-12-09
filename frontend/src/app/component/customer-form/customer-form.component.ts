a9b998ff0f90fc58a99a9717db7bba015b1c6716
KTsybulka <ktsybulka11@gmail.com>
2024-12-08 19:15:23 -0500

    Add customer form component for adding customer data

--- frontend/src/app/component/customer-form/customer-form.component.ts
+++ frontend/src/app/component/customer-form/customer-form.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-customer-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.css'
 })
 export class CustomerFormComponent {
+  feedbackForm : FormGroup;
+  submittedData: any | null = null;
+  apiCustomerUrl = 'http://localhost:8081/addCustomer';
 
+  constructor(private http: HttpClient, private formBuilder: FormBuilder){
+    this.feedbackForm = this.formBuilder.group({
+      customerName: ['', [Validators.required]],
+      customerPassword: [ '', [Validators.required]],
+      active: [ true ],
+      role: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.feedbackForm.valid) {
+      this.submittedData = this.feedbackForm.value;
+
+      this.http.post(this.apiCustomerUrl, this.feedbackForm.value).subscribe(
+        (response) => {
+          console.log('Form submitted', response);
+          alert('Form submitted successfully');
+        },
+        (error) => {
+          console.error('Error in form submission', error);
+          alert('Error in form submission');
+        }
+      );
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
+
