cb6f5adfc7157982033f8bef5fc8139302afe577
KTsybulka <ktsybulka11@gmail.com>
2024-12-14 08:54:25 -0500

    Add JwtAuthenticationFilter class for processing JWT authentication

--- customer-role-service/src/main/java/com/example/customer_role_service/securityConfig/JwtAuthenticationFilter.java
+++ customer-role-service/src/main/java/com/example/customer_role_service/securityConfig/JwtAuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.example.customer_role_service.securityConfig;
 import com.example.customer_role_service.service.JwtService;
 //import io.jsonwebtoken.Claims;
 //import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,42 +18,45 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
-public class JwtAuthenticationFilter{
-//public class JwtAuthenticationFilter  extends OncePerRequestFilter {
-
-//    private final JwtService jwtService;
 //
-//    public JwtAuthenticationFilter(JwtService jwtService) {
-//        this.jwtService = jwtService;
-//    }
+//public class JwtAuthenticationFilter{
 //
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-//            throws ServletException, IOException {
+//
+//}
+public class JwtAuthenticationFilter  extends OncePerRequestFilter {
 
-//        String authHeader = request.getHeader("Authorization");
-//        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-//            String token = authHeader.substring(7);
-//            try {
-//                String username = jwtService.extractUsername(token);
-//                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//                    Claims claims = Jwts.parser()
-//                            .setSigningKey(jwtService.getSecretKey())
-//                            .parseClaimsJws(token)
-//                            .getBody();
-//
-//                    List<String> roles = List.of(claims.get("roles").toString().split(","));
-//                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-//                            username, null, roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
-//                    );
-//                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//                }
-//            } catch (Exception e) {
-//                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//                return;
-//            }
-//        }
-//        chain.doFilter(request, response);
-//    }
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                String username = jwtService.extractUsername(token);
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    Claims claims = Jwts.parser()
+                            .setSigningKey(jwtService.getSecretKey())
+                            .parseClaimsJws(token)
+                            .getBody();
+
+                    List<String> roles = List.of(claims.get("roles").toString().split(","));
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            username, null, roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
 }
