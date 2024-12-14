964873bb860587acfb2c55ba4b601183ebb78bfb
KTsybulka <ktsybulka11@gmail.com>
2024-12-13 21:31:11 -0500

    Update JwtService: Enhance token generation and validation logic

--- customer-role-service/src/main/java/com/example/customer_role_service/service/JwtService.java
+++ customer-role-service/src/main/java/com/example/customer_role_service/service/JwtService.java
@@ -10,41 +10,41 @@ import java.util.Map;
 @Data
 @Service
 public class JwtService {
-//    private static final String SECRET_KEY = "ghsdhsrthety4356345632dsdg325";
-//
-//    public String generateToken(String username, Map<String, Object> claims) {
-//        return Jwts.builder()
-//                .setClaims(claims)
-//                .setSubject(username)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours expiration
-//                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-//                .compact();
-//    }
-//
-//    public String extractUsername(String token) {
-//        return Jwts.parser()
-//                .setSigningKey(SECRET_KEY)
-//                .parseClaimsJws(token)
-//                .getBody()
-//                .getSubject();
-//    }
-//
-//    public boolean validateToken(String token, String username) {
-//        return extractUsername(token).equals(username) && !isTokenExpired(token);
-//    }
-//
-//    private boolean isTokenExpired(String token) {
-//        return Jwts.parser()
-//                .setSigningKey(SECRET_KEY)
-//                .parseClaimsJws(token)
-//                .getBody()
-//                .getExpiration()
-//                .before(new Date());
-//    }
-//
-//    public String getSecretKey() {
-//        return SECRET_KEY;
-//    }
+    private static final String SECRET_KEY = "MY_SECRET_KEY";
+
+    public String generateToken(String username, Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours expiration
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String getSecretKey() {
+        return SECRET_KEY;
+    }
 
 }
